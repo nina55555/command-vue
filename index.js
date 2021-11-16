53a343e83d3f967e5b3d60638f494978bf8caa15
Nina <morville5audrey@gmail.com>
2021-11-16 22:00:09 +0100

    add order buttons

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-
+/*
 Vue.component('fiche-produit',{
     template:`<p class="paragraph">demo-produit</p>`,
 }),
 
 Vue.component('fiche-commande',{
-    template: `<p class="paragraph">demo-commande {{nom}}</p>`,
+    template: `<p class="paragraph">demo-commande </p>`,
 })
-
+*/
 const app = new Vue( {
     el: "#app",
     data: {
         price: "5000e",
-        produits: ["model1", "model2","model3"],
+        produits: ["model1", "model2", "model3"],
         commandes: [],
     },
-    methods: {
-        commander: function (produit){
-            this.commandes.push(produit)
-        }  
-    },
+    methods:{
+        commander: function(produit){
+            console.log(produit)
+            this.commandes.push(produit);
+        }
+    }
     
 })
